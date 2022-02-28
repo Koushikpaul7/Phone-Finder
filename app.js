eb5e7ff44433e506a839d83cbcbf5530ddfedef8
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-02-28 23:25:54 +0600

    data load

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('connect')
+const loadPhones=()=>{
+    const searchField=document.getElementById('search-phone');
+    searchPhone=searchField.value;
+    fetch(`https://openapi.programming-hero.com/api/phones?${searchPhone}`)
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+}
