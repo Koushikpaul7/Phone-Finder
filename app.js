b87b1c0dc5f99954cc86ef629fb8f400c61aa157
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-03-01 00:20:45 +0600

    phones api loaded

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const loadPhones=()=>{
     const searchField=document.getElementById('search-phone');
-    searchPhone=searchField.value;
-    fetch(`https://openapi.programming-hero.com/api/phones?${searchPhone}`)
+    const searchPhone=searchField.value;
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`)
     .then(res=>res.json())
-    .then(data=>console.log(data));
+    .then(data=>displayPhones(data.data));
+}
+const displayPhones= phones =>{
+    console.log(phones);
+   
+    phones.forEach(phone=>{
+        const searchResult= document.getElementById("search-result");
+        const div=document.createElement('div');
+        div.innerHTML=`
+  <div class="card">
+    <img class="w-25" src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.brand} </h5>
+      <p class="card-text">${phone.phone_name}</p>
+      <button class="btn-success">See details</button>
+    </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
