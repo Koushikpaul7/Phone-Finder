8efb4213a288d82424d81f1204e1e0431b2c98f8
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-03-01 20:00:37 +0600

    error handling done

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const loadPhones=()=>{
     document.getElementById("search-result").innerHTML='';
+    document.getElementById("phone-details").innerHTML=''
     const searchField=document.getElementById('search-phone');
     const searchPhone=searchField.value;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`)
     
     .then(res=>res.json())
-    .then(data=>displayPhones(data.data));
+    .then(data=>{
+        if(data.data.length===0||searchPhone==""){
+            document.getElementById('error').style.display="block";
+        }
+        else{
+            document.getElementById('error').style.display="none";
+            displayPhones(data.data)
+        }
+    });
     searchField.value='';
 }
 const displayPhones= phones =>{
@@ -16,7 +25,7 @@ const displayPhones= phones =>{
         const div=document.createElement('div');
         div.innerHTML=`
   <div id="result-style" class="card m-3 text-center">
-    <img class="w-25" src="${phone.image}" class="card-img-top" alt="...">
+    <img class="w-25 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${phone.brand} </h5>
       <p class="card-text">${phone.phone_name}</p>
@@ -40,15 +49,15 @@ const dispayDetails=data=>{
     <div id="detail-style" class="card w-50 mx-auto g-4 mb-4 p-3">
     <img class="w-25 mx-auto" src="${data.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Brand:${data.brand}</h5>
-      <h5 class="card-title">Name:${data.name}</h5>
-      <p class="card-text">Release Date:${data?.releaseDate}</p>
-      <p class="card-text">Display:${data.mainFeatures.displaySize}</p>
-      <p class="card-text"><strong>Memory:</strong>${data.mainFeatures.memory}</p>
-      <p class="card-text">Chipset:${data.mainFeatures.chipSet}</p>
-      <p class="card-text">Others:
-      ${data?.others?.Bluetooth?data.others.Bluetooth:'no data'}  ${data?.others?.GPS?data.others.GPS:'no data'} ${data?.others?.WLAN?data.others.WLAN:'no data'}</p>
-      <p class="card-text">Sensors:${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}</p>
+      <h5 class="card-title">Brand: ${data.brand}</h5>
+      <h5 class="card-title">Name: ${data.name}</h5>
+      <p class="card-text">Release Date: ${data?.releaseDate?data.releaseDate:'released date not found'}</p>
+      <p class="card-text">Display: ${data.mainFeatures.displaySize}</p>
+      <p class="card-text"><strong>Memory:</strong> ${data.mainFeatures.memory}</p>
+      <p class="card-text">Chipset: ${data.mainFeatures.chipSet}</p>
+      <p class="card-text">Others: 
+      ${data?.others?.Bluetooth?data.others.Bluetooth:'no data found'}  ${data?.others?.GPS?data.others.GPS:''} ${data?.others?.WLAN?data.others.WLAN:''}</p>
+      <p class="card-text">Sensors: ${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}</p>
     </div>
     </div>
     `;
