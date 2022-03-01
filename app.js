3205324b07a6374801197ed6e45b46214d3a2548
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-03-01 13:17:42 +0600

    clear fields

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const loadPhones=()=>{
+    document.getElementById("search-result").innerHTML='';
     const searchField=document.getElementById('search-phone');
     const searchPhone=searchField.value;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`)
+    
     .then(res=>res.json())
     .then(data=>displayPhones(data.data));
+    searchField.value='';
 }
 const displayPhones= phones =>{
     console.log(phones);
@@ -30,16 +33,23 @@ const loadDetails=(info)=>{
 }
 const dispayDetails=data=>{
     console.log(data);
+    document.getElementById("phone-details").innerHTML='';
     const phoneDetails=document.getElementById("phone-details");
     const div=document.createElement('div');
-    div.classList.add('card');
     div.innerHTML=`
-    
-    <img w-25 src="..." class="card-img-top" alt="...">
+    <div class="card w-50 mx-auto g-4 mb-4 p-3">
+    <img class="w-25 mx-auto" src="${data.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${data.brand}</h5>
-      <h5 class="card-title">${data.name}</h5>
-      <p class="card-text">${data.releaseDate}</p>
+      <h5 class="card-title">Brand:${data.brand}</h5>
+      <h5 class="card-title">Name:${data.name}</h5>
+      <p class="card-text">Release Date:${data?.releaseDate}</p>
+      <p class="card-text">Display:${data.mainFeatures.displaySize}</p>
+      <p class="card-text"><strong>Memory:</strong>${data.mainFeatures.memory}</p>
+      <p class="card-text">Chipset:${data.mainFeatures.chipSet}</p>
+      <p class="card-text">Others:
+      ${data?.others?.Bluetooth}, ${data?.others?.GPS},${data?.others?.WLAN}</p>
+      <p class="card-text">Sensors:${data.mainFeatures.sensors[0]},${data.mainFeatures.sensors[1]},${data.mainFeatures.sensors[2]},${data.mainFeatures.sensors[3]},${data.mainFeatures.sensors[4]},${data.mainFeatures.sensors[5]}</p>
+    </div>
     </div>
     `;
     phoneDetails.appendChild(div);
